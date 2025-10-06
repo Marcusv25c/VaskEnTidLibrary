709a74648adeba94ad91ad7f92798535a1a618b5
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-10-06 12:24:04 +0200

    oprettet booking og laundromat klasserSQL Nuget

--- VaskEnTidLibrary/Model/Laundromat.cs
+++ VaskEnTidLibrary/Model/Laundromat.cs
@@ -8,5 +8,42 @@ namespace VaskEnTidLibrary.Model
 {
     public class Laundromat
     {
+        private int _laundromatID;
+        private string _adress;
+        private string _zipcode;
+        private string _city;
+
+        public int LaundromatID
+        {
+            get { return _laundromatID; }
+            set { _laundromatID = value; }
+        }
+
+        public string Adress
+        {
+            get { return _adress; }
+            set { _adress = value; }
+        }
+
+        public string Zipcode
+        {
+            get { return _zipcode; }
+            set { _zipcode = value; }
+        }
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public Laundromat(int laundromatID, string adress, string zipcode, string city)
+        {
+            _laundromatID = laundromatID;
+            _adress = adress;
+            _zipcode = zipcode;
+            _city = city;
+        }
+
     }
 }
